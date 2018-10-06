18cfe4c8bd149ba5a7ed7447dc2b1798b5c18dbc
kevin tauzer <kevintauzer97@gmail.com>
2018-10-06 04:04:34 -1000

    yup, i'm doing it right

--- exercises.js
+++ exercises.js
@@ -119,7 +119,14 @@ console.log(bigBird('dog'));
  * If the strings are equal, the function will return the message "You look mahvelous!" Otherwise, return the message: "I don't know who you are anymore." 
  * Console.log your result.
 */
-
+function isEqual(first, second){
+  if(first === second){
+  return 'you look mahvelous!'
+}else{
+ return "i don't know who you are anymore"
+  }
+}
+console.log(isEqual("three", "6"));
 
 /*
  * #7
