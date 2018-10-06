7ce51436b151d50f02423b822d823d7e139d6250
kevin tauzer <kevintauzer97@gmail.com>
2018-10-06 04:20:10 -1000

    i changed it back to static, i like it more, also... howzit cuz

--- exercises.js
+++ exercises.js
@@ -159,8 +159,14 @@ console.log(notEqual('1', '1'));
  * The function will return true if the number passed into the function is greater than 100, otherwise it will return false.
  * Console.log your result.
 */ 
-
-
+function spareChange(money){
+  if(money >= 100){
+    return true + " get money cuz";
+  }else{
+    return false + " we no money cuz";
+  }
+}
+console.log(spareChange(398712639786));
 
 /*
  * #9
