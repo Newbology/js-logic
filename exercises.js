7810068f7929d43ce02720ccb22c5d38f6f920ab
kevin tauzer <kevintauzer97@gmail.com>
2018-10-06 23:13:29 -1000

    fave foods for now

--- exercises.js
+++ exercises.js
@@ -365,7 +365,9 @@ for (var i=0; i<players.length; i++){
  * Create a for loop that will iterate and console.log each item in the array below:
 */
   var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
-
+ for (var i=0; i<myFavFoods.length; i++){
+   console.log('Fave Food for now '+myFavFoods[i]);
+ }
 
 /*
  * #17
